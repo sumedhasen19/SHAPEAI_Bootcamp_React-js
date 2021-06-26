6387731e7e55f0cbbd86bc6709947692b21d259f
sumedhasen19 <86519223+sumedhasen19@users.noreply.github.com>
2021-06-26 17:35:48 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <p>SHAPE AI</p>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1> JAVA SCRIPT AND REACT.JS </h1>
+      <p>A basic web dev React js bootcamp</p>
+    </div>
+  );
+}
+
+export default Info;
